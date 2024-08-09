95fbe247571a8e3bb9562adeaea45c56afee32d6
TOMAS NEIMERMAN GROSSO <47496739@ort>
2024-08-09 12:03:13 -0300

    avances

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-const { app, BrowserWindow } = require('electron/main')
+const { app, BrowserWindow, shell } = require('electron/main')
 const path = require('node:path')
 
+if (process.defaultApp) {
+  if (process.argv.length >= 2) {
+    app.setAsDefaultProtocolClient('electron-fiddle', process.execPath, [path.resolve(process.argv[1])])
+  }
+} else {
+  app.setAsDefaultProtocolClient('electron-fiddle')
+}
 function createWindow () {
   const win = new BrowserWindow({
     width: 1280,
@@ -11,6 +18,26 @@ function createWindow () {
   })
 
   win.loadFile('index.html')
+}
+const gotTheLock = app.requestSingleInstanceLock()
+
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+    // the commandLine is array of strings in which last element is deep link url
+    dialog.showErrorBox('Welcome Back', `You arrived from: ${commandLine.pop()}`)
+  })
+
+  // Create mainWindow, load the rest of the app, etc...
+  app.whenReady().then(() => {
+    createWindow()
+  })
 }
 app.whenReady().then(() => {
   createWindow()
--- preload.js
+++ preload.js
@@ -1,18 +1,8 @@
-const {ipcRenderer} = require('electron')
+const { contextBridge, ipcRenderer } = require('electron')
+const path = require('node:path')
 
-let usuario;
-let contrasena;
-
-window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-      const element = document.getElementById(selector)
-      if (element) element.innerText = text
-    }
-  
-    for (const type of ['chrome', 'node', 'electron']) {
-      replaceText(`${type}-version`, process.versions[type])
-    }
-  })
-/*window.onload = function() {
-   = document.getElementById(username)
-}*/
+contextBridge.exposeInMainWorld('electron', {
+  startDrag: (fileName) => {
+    ipcRenderer.send('ondragstart', path.join(process.cwd(), fileName))
+  }
+})
